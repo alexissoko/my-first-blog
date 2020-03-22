103ebad79eebaab1b8f116aa3015ca626cf1ce2f
alexissoko <alesoko@live.com>
2020-03-22 10:33:02 -0700

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 't_-(dbgl$93iqf*8uh)k9#gejpeusm&=v!2n7h@6pxa1-qc36p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'www.tautologico.com']
 
 
 # Application definition
