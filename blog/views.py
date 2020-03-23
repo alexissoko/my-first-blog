ff483119215d0f756fb48f831ed72fc15de74c31
alexissoko <alesoko@live.com>
2020-03-22 18:53:55 -0700

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,8 @@ from .models import Post
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.all()
+    #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 # first version
