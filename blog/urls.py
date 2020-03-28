f113aa97f7a474e5194642f3ecbb009d1979ebc9
alexissoko <alesoko@live.com>
2020-03-28 10:50:38 -0700

    Changed the HTML for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -6,8 +6,4 @@ urlpatterns = [
     path('post/<int:pk>/', post_detail, name='post_detail'),
     path('post/new/', post_new, name='post_new'),
     path('post/<int:pk>/edit/', post_edit, name='post_edit'),
-    path('post/about/', about_view, name='about'),
-    path('post/contact/', about_view, name='about'),
-    path('post/stack/', stack_view, name='stack'),
-    path('post/contact/', contact_view, name='contact'),
 ]
