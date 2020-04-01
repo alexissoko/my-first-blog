477fe84f4dd9da6d11f3de46fe3d0824daf3bc2a
alexissoko <alesoko@live.com>
2020-04-01 08:05:46 -0700

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,4 +120,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = "/home/alexissoko/alexissoko.pythonanywhere.com/static"
+STATIC_ROOT = "/home/alexissoko/alexissoko/static"
