caea423425ea0c60dcd700d71f1deef14bbf57c4
alexissoko <alesoko@live.com>
2020-04-07 16:42:46 -0700

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -5,15 +5,15 @@ from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 
 
-def post_list(request):
+def services_view(request):
     posts = Post.objects.all()
     #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/services.html', {'posts': posts})
 
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    return render(request, 'blog/services.html', {'post': post})
 
 
 def post_new(request):
